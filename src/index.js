3ccc98dbcacc6da7e44a88734c238422445ccb25
Andrei Paleshka <andrepoleshko@gmail.com>
2023-01-17 01:04:56 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,45 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+// function decode(expr) {
+//     const encode = {
+//         '10': '.',
+//         '11':  '-',
+//     };
+
+//     const zero = '0';
+//     const getKeyByValue = (object, value) => Object.keys(object).find(key => object[key] === value);
+
+//     return expr.split("").map(x => getKeyByValue(MORSE_TABLE, x)).map(x => x !== undefined ? x.split("").map(x => getKeyByValue(encode, x)) : ['**********']).map(x => (x.length > 1) ? (x.reduce((a, b) => a + b)) : x).flat().map(x => (x.length < 10) ? (zero.repeat(10 - x.length) + x) : x).reduce((a,b) => a + b);
+
+// }
+
 function decode(expr) {
-    // write your solution here
+        const encode = {
+            '.': '10',
+            '-':  '11',
+            ' ': '**********',
+        };
+
+        const getKeyByValue = (object, value) => Object.keys(object).find(key => object[key] === value);
+
+        const splitArray = (str, multiple) => {
+            let start = [];
+            for (let i = 0; i < str.length; i += multiple) {
+                const part = str.slice(i, i + multiple);
+                start.push(part);
+            }
+            return start;
+        }
+
+        let arr1 = splitArray(expr, 10).map(x => splitArray(x, 2)).map(x => splitArray(x, 1));
+        let arr2 = [];
+        for (let i = 0; i < arr1.length; i++) {
+            const filter = arr1[i].filter(x => x != '00').flat().map(x => getKeyByValue(encode, x)).reduce((a,b) => a + b);
+            arr2.push(filter);
+        }
+
+        return arr2.map(x => MORSE_TABLE[x]).map(x => x === undefined ? ' ' : x).reduce((a, b) => a + b);
 }
 
 module.exports = {
