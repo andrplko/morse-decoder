923aa51e52fab87472ab95498bbfc33ba778b3b1
Andrei Paleshka <andrepoleshko@gmail.com>
2023-10-17 19:42:45 +0200

    refactor: improve function implementation

--- src/index.js
+++ src/index.js
@@ -37,45 +37,18 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-// function decode(expr) {
-//     const encode = {
-//         '10': '.',
-//         '11':  '-',
-//     };
-
-//     const zero = '0';
-//     const getKeyByValue = (object, value) => Object.keys(object).find(key => object[key] === value);
-
-//     return expr.split("").map(x => getKeyByValue(MORSE_TABLE, x)).map(x => x !== undefined ? x.split("").map(x => getKeyByValue(encode, x)) : ['**********']).map(x => (x.length > 1) ? (x.reduce((a, b) => a + b)) : x).flat().map(x => (x.length < 10) ? (zero.repeat(10 - x.length) + x) : x).reduce((a,b) => a + b);
-
-// }
-
 function decode(expr) {
-        const encode = {
-            '.': '10',
-            '-':  '11',
-            ' ': '**********',
-        };
+  let slicedArray= [];
 
-        const getKeyByValue = (object, value) => Object.keys(object).find(key => object[key] === value);
+  for (let i = 0; i < expr.length; i += 10) {
+    slicedArray.push(expr.slice(i, i + 10));
+  }
 
-        const splitArray = (str, multiple) => {
-            let start = [];
-            for (let i = 0; i < str.length; i += multiple) {
-                const part = str.slice(i, i + multiple);
-                start.push(part);
-            }
-            return start;
-        }
-
-        let arr1 = splitArray(expr, 10).map(x => splitArray(x, 2)).map(x => splitArray(x, 1));
-        let arr2 = [];
-        for (let i = 0; i < arr1.length; i++) {
-            const filter = arr1[i].filter(x => x != '00').flat().map(x => getKeyByValue(encode, x)).reduce((a,b) => a + b);
-            arr2.push(filter);
-        }
-
-        return arr2.map(x => MORSE_TABLE[x]).map(x => x === undefined ? ' ' : x).reduce((a, b) => a + b);
+  return slicedArray
+    .map(el => el.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-'))
+    .map(el => MORSE_TABLE[el])
+    .map(el => !el ? ' ' : el)
+    .join('');
 }
 
 module.exports = {
